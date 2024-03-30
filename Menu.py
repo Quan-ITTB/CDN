75298de8b956cfc548ea42d1cbff600fe4dec09c
Quan <quanittb@gmail.com>
2024-03-30 09:25:28 +0700

    tab

--- Menu.py
+++ Menu.py
@@ -81,49 +81,43 @@ class Ui_MainWindow(object):
 # code sử lý sự kiện để hiện file menu
 
 class MenuTong(QMainWindow,Ui_MainWindow ):
-    def __init__(self):
-        super().__init__()
-        self.setupUi(self) 
-# code thoát chương trình 
-        self.btn_exit.clicked.connect(QApplication.instance().exit)
-# xử lý sự kiện khi click vào DS Bán hàng trang đó sẽ hiện lên
-        self.btn_qlbh.clicked.connect(self.show_DSBH) 
-        self.btn_qlnv.clicked.connect(self.show_DSNV) 
-# xử lý sự kiện khi click vào DS Bán hàng trang đó sẽ hiện lên
-        self.btn_qlnh.clicked.connect(self.show_DSNH) 
-
-        
-# xử lý sự kiện khi click vào DS Bảo hành trang đó sẽ hiện lên
-        self.btn_qlbhanh.clicked.connect(self.show_DSBaoHanh) 
-
+        def __init__(self):
+                super().__init__()
+                self.setupUi(self) 
+        # code thoát chương trình 
+                self.btn_exit.clicked.connect(QApplication.instance().exit)
+        # xử lý sự kiện khi click vào DS Bán hàng trang đó sẽ hiện lên
+                self.btn_qlbh.clicked.connect(self.show_DSBH) 
+                self.btn_qlnv.clicked.connect(self.show_DSNV) 
+        # xử lý sự kiện khi click vào DS Bán hàng trang đó sẽ hiện lên
+                self.btn_qlnh.clicked.connect(self.show_DSNH)      
+        # xử lý sự kiện khi click vào DS Bảo hành trang đó sẽ hiện lên
+                self.btn_qlbhanh.clicked.connect(self.show_DSBaoHanh) 
+                self.dsnv_window = NhanVien()
 # code tab bán hàng
-    def show_DSBH(self):
-        # Tạo một instance của trang DSBH
-        self.dsbh_window = Banhang()
-        # Hiển thị trang DSBH
-        self.dsbh_window.show()
-        # self.dsbh_window.closeEvent.connect(self.show_Menu)   
-    def show_Menu(self):
-        self.show()
-
-    def show_DSNV(self):
-        # Tạo một instance của trang DSBH
-        self.dsnv_window = NhanVien()
-        # Hiển thị trang DSBH
-        self.dsnv_window.show()
-        # self.dsbh_window.closeEvent.connect(self.show_Menu) 
-# code tab nhập hàng hàng
-    def show_DSNH(self):
-        # Tạo một instance của trang DSBH
-        self.dsnh_window = Nhaphang()
-        self.dsnh_window.show()
-        # Hiển thị trang DSBH
-        # code tab bảo hành 
-    def show_DSBaoHanh(self):
-        # Tạo một instance của trang DSBH
-        self.dsbaohanh_window = Baohanh()
-        # Hiển thị trang DSBH
-        self.dsbaohanh_window.show() 
+        def show_DSBH(self):
+                # Tạo một instance của trang DSBH
+                self.dsbh_window = Banhang()
+                # Hiển thị trang DSBH
+                self.dsbh_window.show()
+                # self.dsbh_window.closeEvent.connect(self.show_Menu)   
+        def show_Menu(self):
+                self.show()
+        def show_DSNV(self):
+                if not self.dsnv_window.isVisible():  # Kiểm tra xem cửa sổ Home_Window đang không hiển thị
+                        self.dsnv_window.show()     
+        # code tab nhập hàng hàng
+        def show_DSNH(self):
+                # Tạo một instance của trang DSBH
+                self.dsnh_window = Nhaphang()
+                self.dsnh_window.show()
+                # Hiển thị trang DSBH
+                # code tab bảo hành 
+        def show_DSBaoHanh(self):
+                # Tạo một instance của trang DSBH
+                self.dsbaohanh_window = Baohanh()
+                # Hiển thị trang DSBH
+                self.dsbaohanh_window.show()     
 class NhanVien(QMainWindow, Ui_AgentWindow):
     def __init__(self):
         super().__init__()
@@ -137,18 +131,16 @@ class NhanVien(QMainWindow, Ui_AgentWindow):
         self.WidgetDSNV.setRowCount(len(results))
         tablerow =0
         for row in results: 
-           self.WidgetDSNV.setItem(tablerow, 0, QTableWidgetItem(str(row[0])))
-           self.WidgetDSNV.setItem(tablerow, 1, QTableWidgetItem(str(row[1])))
-           self.WidgetDSNV.setItem(tablerow, 2, QTableWidgetItem(str(row[2])))
-           self.WidgetDSNV.setItem(tablerow, 3, QTableWidgetItem(str(row[3])))
-           self.WidgetDSNV.setItem(tablerow, 4, QTableWidgetItem(str(row[4])))
-           self.WidgetDSNV.setItem(tablerow, 5, QTableWidgetItem(str(row[5])))
-           self.WidgetDSNV.setItem(tablerow, 6, QTableWidgetItem(str(row[6])))
-           self.WidgetDSNV.setItem(tablerow, 7, QTableWidgetItem(str(row[7])))
-           self.WidgetDSNV.setItem(tablerow, 8, QTableWidgetItem(str(row[8])))
-           tablerow += 1
-
-
+                self.WidgetDSNV.setItem(tablerow, 0, QTableWidgetItem(str(row[0])))
+                self.WidgetDSNV.setItem(tablerow, 1, QTableWidgetItem(str(row[1])))
+                self.WidgetDSNV.setItem(tablerow, 2, QTableWidgetItem(str(row[2])))
+                self.WidgetDSNV.setItem(tablerow, 3, QTableWidgetItem(str(row[3])))
+                self.WidgetDSNV.setItem(tablerow, 4, QTableWidgetItem(str(row[4])))
+                self.WidgetDSNV.setItem(tablerow, 5, QTableWidgetItem(str(row[5])))
+                self.WidgetDSNV.setItem(tablerow, 6, QTableWidgetItem(str(row[6])))
+                self.WidgetDSNV.setItem(tablerow, 7, QTableWidgetItem(str(row[7])))
+                self.WidgetDSNV.setItem(tablerow, 8, QTableWidgetItem(str(row[8])))
+                tablerow += 1
 app = QApplication (sys.argv) 
 Widget = QtWidgets.QStackedWidget()
 MenuT1 = MenuTong() 
